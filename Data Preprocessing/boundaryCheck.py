907c0cf7913c3e716da97ae3d9ff2ed84ce16f4b
Pranav <pranav.bhawan@gmail.com>
2022-08-17 19:23:04 +0530

    Improvised user boundary lat lng mapping script

--- Data Preprocessing/boundaryCheck.py
+++ Data Preprocessing/boundaryCheck.py
@@ -10,12 +10,18 @@ def boundaryFilling():
         lng = df.loc[i, 'Longitude']
         lat = df.loc[i, 'Latitude']
         # print(lat,lng)
-        checkBoundary(str(lat), str(lng))
+        stationID = checkBoundary(str(lat), str(lng))
+        df.loc[i,'StationID'] = stationID
+        if(i%1000==0):
+          df.to_csv('out.csv')
 
 def checkBoundary(lat,lng):
   result = subprocess.run(['node', 'boundaryCheck.js',lat,lng], stdout=subprocess.PIPE).stdout.decode('utf-8')
   
   # It is 1 if the point is inside any of the polygon, else it is 0
   if(result[0]!='0'):
-    print('Station ID:',result[2:])
+    # print('Station ID:',result[2:])
+    return result[2:].strip()
+  return -1
+
 boundaryFilling()
