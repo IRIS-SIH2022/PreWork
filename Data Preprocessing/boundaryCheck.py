9f3e5a35e8d1aea149baaeb809880fad094bda8b
Sasank <gssasank@gmail.com>
2022-08-19 23:59:03 +0530

    Station boundary set in out.csv 🚓

--- Data Preprocessing/boundaryCheck.py
+++ Data Preprocessing/boundaryCheck.py
@@ -2,18 +2,18 @@ import pandas as pd
 import numpy as np
 import subprocess
 
-df = pd.read_csv("bigData.csv")
+df = pd.read_csv("data2.csv")
 
 
 def boundaryFilling():
     for i in range(0, len(df)):
-        lng = df.loc[i, 'Longitude']
-        lat = df.loc[i, 'Latitude']
+        lng = df.loc[i, 'lng']
+        lat = df.loc[i, 'lat']
         # print(lat,lng)
         stationID = checkBoundary(str(lat), str(lng))
         df.loc[i,'StationID'] = stationID
         if(i%1000==0):
-            print("1000 added")
+            print(f"{i} * 1000 added; {100000-i} left")
             df.to_csv('out.csv')
 
 def checkBoundary(lat,lng):
